c8853002d4d2e7fe4661760e5fb5a91d6fd37536
Won <dmsahfo111@naver.com>
2023-09-25 18:47:56 +0900

    project 1

---
+++ src/main/java/com/example/account/dto/CancelBalance.java
@@ -0,0 +1,64 @@
+package com.example.account.dto;
+
+import com.example.account.aop.AccountLockIdInterface;
+import com.example.account.type.TransactionResultType;
+import lombok.*;
+
+import javax.validation.constraints.*;
+import java.time.LocalDateTime;
+
+public class CancelBalance {
+    /**
+     * "transactionId":,
+     * "accountNumber":"1000000000",
+     * "amount":1000
+     */
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Request implements AccountLockIdInterface {
+        @NotBlank
+        private String transactionId;
+
+        @NotBlank
+        @Size(min = 10, max = 10)
+        private String accountNumber;
+
+        @NotNull
+        @Min(10)
+        @Max(1000_000_000)
+        private Long amount;
+    }
+
+    /**
+     * "accountNumber":"1234567890",
+     * "transactionResult":"S",
+     * "transactionId":"c2033bb6d82a4250aecf8e27c49b63f6",
+     * "amount":1000,
+     * "transactedAt":"2022-06-01T23:26:14.671859"
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response{
+        private String accountNumber;
+        private TransactionResultType transactionResult;
+        private String transactionId;
+        private  Long amount;
+        private LocalDateTime transactedAt;
+
+        public static Response from(TransactionDto transactionDto) {
+
+            return Response.builder()
+                    .accountNumber(transactionDto.getAccountNumber())
+                    .transactionResult(transactionDto.getTransactionResultType())
+                    .transactionId(transactionDto.getTransactionId())
+                    .transactedAt(transactionDto.getTransactedAt())
+                    .amount(transactionDto.getAmount())
+                    .build();
+        }
+    }
+}
